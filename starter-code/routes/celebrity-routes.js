6fafdccc7b470bb20ce6b8bda0feb549f6ded5c8
Hans Garcia <hansgarcia96@gmail.com>
2019-08-17 14:20:55 -0400

    Working Verion with details, working on edit

--- starter-code/routes/celebrity-routes.js
+++ starter-code/routes/celebrity-routes.js
@@ -2,29 +2,48 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/celebrity"); // import the celebrity from the models folder
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+// GET - HOMEPAGE
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-// GET - to display the form for Creating the Celebrity
+// GET - NEW CELEBRITY FORM
 router.get("/celebrities/new", (req, res, next) => {
-    res.render("celebrities/new-celebrity")
-  });
-
-router.post("/celebrities/create", (req, res, next) => {
-console.log("THE FORM ", req.body);
-Celebrity
-    .create(req.body)
-    .then( newCelebrity => res.redirect("/celebrity"))
-    .catch(err => console.log('error creating new celebrity'))
+  res.render("celebrities/new-celebrity");
 });
 
+// POST - NEW CELEBRITY 
+router.post("/celebrities/create", (req, res, next) => {
+  console.log("THE FORM ", req.body);
+  Celebrity.create(req.body)
+    .then(newCelebrity => res.redirect("/celebrities"))
+    .catch(err => console.log("error creating new celebrity"));
+});
+
+// GET - ALL ADDED CELEBRITIES
 router.get("/celebrities", (req, res, next) => {
-  Celebrity
-    .find()
-    .then(celebritiesFromDB => res.render("celebrities/celebrities", {celebrities: celebritiesFromDB}))
-    .catch(err => console.log("Error getting the celerities from the DB: ", err))
-})
+  Celebrity.find()
+    .then(celebritiesFromDB =>
+      res.render("celebrities/celebrities", { celebrities: celebritiesFromDB })
+    )
+    .catch(err =>
+      console.log("Error getting the celerities from the DB: ", err)
+    );
+});
+
+// GET - CELEBRITY DETAILS
+router.get("/celebrities/celebrity-details/:id", (req, res, next) => {
+  const theID = req.params.id;
+
+  Celebrity.findById(theID)
+    .then(result => {
+      res.render("celebrities/celebrity-details", { singleCelebrity: result });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
 
 module.exports = router;
